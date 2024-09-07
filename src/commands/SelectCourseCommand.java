196680425c5ee1a8d2ef013a85a6f6c4304bfe0a
= <=>
2024-09-07 12:09:06 +0800

    Forget to add .java

---
+++ src/commands/SelectCourseCommand.java
@@ -0,0 +1,22 @@
+package commands;
+
+import exceptions.ExecutionException;
+import exceptions.ValidationException;
+
+/**
+ * &#064;Classname SelectCourseCommand
+ * &#064;Description  TODO
+ * &#064;Date 2024/9/4 19:32
+ * &#064;Created MuJue
+ */
+public class SelectCourseCommand extends BaseCommand{
+    @Override
+    public void execute() throws ExecutionException {
+
+    }
+
+    @Override
+    public void validate() throws ValidationException {
+
+    }
+}
